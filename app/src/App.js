d3009fc11ef8fe2dca8d8007115a2134c4f57ef3
GustavoMarks <gustavosmarquesf@gmail.com>
2021-03-26 20:44:45 -0300

    feat: função de criação de aluno

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,23 @@
 import AlunoForm from "./components/AlunoForm";
+import api from "./api";
 
 function App() {
+
+  async function createAluno(data) {
+    try {
+      const res = await api.post("/api/alunos", data);
+      if (!res) return alert("Erro ao tentar criar aluno... Tente novamente mais tarde!");
+    } catch (error) {
+      console.log(error);
+      return alert("Erro ao tentar criar aluno... Tente novamente mais tarde!");
+    }
+  }
+
   return (
     <main>
       <h2> Sistema de Controle de Alunos </h2>
-      <AlunoForm />
+      <AlunoForm sendAluno={createAluno} />
+      <br />
     </main>
   );
 }
